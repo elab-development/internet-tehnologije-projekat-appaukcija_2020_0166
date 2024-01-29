53ec3863b31900c7fdaa850fffeb37a99bd53ac6
Aleksandar Zivanovic <aleksandarzivanovic24@gmail.com>
2024-01-29 21:20:39 +0100

    Formiranje Login stranice

--- Angular-aukcije/src/app/user-login/user-login.component.ts
+++ Angular-aukcije/src/app/user-login/user-login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
-export class UserLoginComponent {
-
+export class UserLoginComponent implements OnInit {
+  signupUsers: any[] = [];
+  signupObj: any = {
+    userName: '',
+    email: '',
+    password: ''
+  };
+  loginObj: any = {
+    email: '',
+    password: ''
+  }
+  constructor() { }
+  ngOnInit(): void {
+    const localData = localStorage.getItem('signUpUsers');
+    if (localData != null) {
+      this.signupUsers = JSON.parse(localData);
+    }
+  }
+  onSignUp() {
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+    this.signupObj = {
+      userName: '',
+      email: '',
+      password: ''
+    }
+  }
+  onLogin() {
+    debugger
+    const IsUserExist = this.signupUsers.find(m => m.email == this.loginObj.email && m.password == this.loginObj.password);
+    if (IsUserExist != undefined) {
+      alert('User Login Successfully');
+    }
+    else {
+      alert('Wrong credentials');
+    }
+  }
 }
