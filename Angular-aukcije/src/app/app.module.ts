0a163e735ed447398669ae8c2ad5057d675e03b8
Aleksandar Zivanovic <aleksandarzivanovic24@gmail.com>
2024-01-29 20:46:08 +0100

    Kreiranje Search komponente i dodavanje search funkcije Home komponenti

--- Angular-aukcije/src/app/app.module.ts
+++ Angular-aukcije/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
